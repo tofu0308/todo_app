158bd6cf8f9c375faa43b5883cc928b7b753a4c9
tofu0308 <isisaka2000@gmail.com>
2018-03-10 17:06:09 +0900

    add todo js

---
+++ actions/index.js
@@ -0,0 +1,27 @@
+//todo id管理
+let nextTodoId = 0;
+
+//add todo
+export const addTodo = (rext) => {
+	return {
+		type: 'ADD_Todo',
+		id: nextTodoId ++,
+		text
+	};
+};
+
+//done todo
+export const toggleTodo = (id) => {
+	return {
+		type: 'TOGGLE_TODO',
+		id
+	};
+};
+
+//filtering todo
+export const setVisibilityFilter = (filter) => {
+	return {
+		type: 'SET_VISIBILTY_FILTER',
+		filter
+	};
+};
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import { addTodo, toggleTodo, setVisibiltyFilter} from './actions/index.js';
 
 let store = createStore(function(){
 	return 'Hello Redux';
@@ -6,3 +7,9 @@ let store = createStore(function(){
 
 const contents = document.getElementById('contents');
 contents.innerHTML = store.getState().toString();
+
+//add todo
+
+//done todo
+
+//filtering todo
