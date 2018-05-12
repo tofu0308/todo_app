6975e21c41b535ade3c2df92df6f608821c7fd91
tofu0308 <isisaka2000@gmail.com>
2018-05-12 17:18:40 +0900

    add app.js / mod index

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-import { createStore } from 'redux';
-import { addTodo, toggleTodo, setVisibiltyFilter} from './actions/index.js';
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import todoApp from './reducer';
+import App from './components/App';
 
 let store = createStore(todoApp);
 
-/*
-const contents = document.querySelector('contents');
-contents.innerHTML = store.getState().toString();
-*/
-
-//add todo
-var addTodoElem = document.querySelector('#appTodo');
-var input = addTodoElem.getElementsByTagName('input')[0];
-var button = addTodoElem.getElementsByTagName('button')[0];
-
-button.addEventListener('click', () => {
- var todoText = input.value;
- store.dispatch(addTodo(todoText));
-});
-
-//done todo
-var todoList = document.querySelector('#todoList');
-var elements = todoList.getElementsByTagName('li');
-var listArray = [...elements];
-
-listArray.forEach((v, index) => {
- v.addEventListener(click, e => {
-  store.dispatch(toggleTodo(index));
- })
-});
-
-//filtering todo
-var links = document.querySelector('#link');
-var childs = links.childNodes;
-var childList = [...childs];
-
-childList.filter(v => v.nodeName != '#text').forEach(v => {
- v.addEventListener('click', e => {
-  var filterText = v.innerHTML;
-  store.dispatch(setVisibiltyFilter(filterText));
- });
-});
-sample
+render (
+	<Provider store={store>
+		<App />
+	</Provider>,
+	document.getElementByID('root')
+);
